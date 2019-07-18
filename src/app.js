644f0a4e20a2182bfe3703db87c863a59df73def
Kenny Claka <kennyigbechi@gmail.com>
2019-07-18 10:36:57 -0700

    Fixed Colors for right and wrong answers

--- src/app.js
+++ src/app.js
@@ -1,6 +1,7 @@
 const gameArea = document.querySelector(".game");
-
 const button = document.querySelector("button");
+const message = document.querySelector(".message");
+
 let gamePlay = false;
 
 button.addEventListener("click", function () {
@@ -14,9 +15,12 @@ button.addEventListener("click", function () {
             console.log(numbers[i].value);
             console.log(numbers[i].correct);
             if (parseInt(numbers[i].value) === numbers[i].correct) {
-                console.log("Yzayy");
+                numbers[i].style.backgroundColor = "green";
+                numbers[i].style.color = "white";
             } else {
-                console.log("No Match");
+               let color = (numbers[i].value < numbers[i].correct) ? "blue" : "red";
+                numbers[i].style.backgroundColor = color;
+                numbers[i].style.color = "black";
             }
         }
 
