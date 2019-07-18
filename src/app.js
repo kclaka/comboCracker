d2a9a887e9ba1f2b3092a0308a993844e75c415f
Kenny Claka <kennyigbechi@gmail.com>
2019-07-18 09:17:55 -0700

    Added Combo Numbers

--- src/app.js
+++ src/app.js
@@ -7,5 +7,24 @@ button.addEventListener("click", function () {
     if(!gamePlay){
         gamePlay = true;
         button.innerHTML = "Check Combo";
+    }else{
+        maker();
     }
 });
+
+const maker = function () {
+    for(let i = 0; i < 4; i++){
+        let el = document.createElement("input");
+        el.setAttribute("type", "number");
+        el.classList.add("numb");
+        el.max = 9;
+        el.min = 0;
+        el.size = 1;
+        el.style.width = "40px";
+        el.value = 0;
+        el.order = 0;
+        console.log(el);
+        gameArea.appendChild(el);
+    }
+
+};
