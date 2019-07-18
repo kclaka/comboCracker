5420a7bf014cb808842f10d95c8c99920fa58c4f
Kenny Claka <kennyigbechi@gmail.com>
2019-07-18 10:11:35 -0700

    fixed game logic

--- src/app.js
+++ src/app.js
@@ -7,9 +7,22 @@ button.addEventListener("click", function () {
     if(!gamePlay){
         gamePlay = true;
         button.innerHTML = "Check Combo";
-    }else{
         maker();
+    }else{
+        const numbers = document.querySelectorAll(".numb")
+        for(let i = 0; i < numbers.length; i++) {
+            console.log(numbers[i].value);
+            console.log(numbers[i].correct)
+            if (parseInt(numbers[i].value) === numbers[i].correct) {
+                console.log("Yzayy")
+            } else {
+                console.log("No Match");
+            }
+        }
+
     }
+
+
 });
 
 const maker = function () {
@@ -20,11 +33,13 @@ const maker = function () {
         el.max = 9;
         el.min = 0;
         el.size = 1;
-        el.style.width = "40px";
-        el.value = 0;
+        el.style.width = "60px";
         el.order = 0;
+        el.correct = Math.floor(Math.random()*10);
+        console.log(el.correct)
+        el.value = 0;
         console.log(el);
         gameArea.appendChild(el);
     }
-
 };
+
