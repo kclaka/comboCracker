1dd2fa9e013ba16a0ce94d5826fd0c02c8afccac
Kenny Claka <kennyigbechi@gmail.com>
2019-07-18 09:03:43 -0700

    Added first button - checked changed

---
+++ src/app.js
@@ -0,0 +1,11 @@
+const gameArea = document.querySelector(".game");
+
+const button = document.querySelector("button");
+let gamePlay = false;
+
+button.addEventListener("click", function () {
+    if(!gamePlay){
+        gamePlay = true;
+        button.innerHTML = "Check Combo";
+    }
+});
